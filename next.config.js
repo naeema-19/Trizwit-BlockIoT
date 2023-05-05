7eea2217c2e9bbc826ee2aa5445a72325314f727
NAEEMA ZIYAD <75922224+naeema-19@users.noreply.github.com>
2023-05-05 17:34:20 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,9 @@ const nextConfig = {
   env: {
     GOOGLE_CLIENT_ID : "690300101234-26hh008rrrkiurushvgo14n1r502tgt9.apps.googleusercontent.com",
     GOOGLE_CLIENT_SECRET : "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",
+    NEXTAUTH_SECRET :"openssl rand -base64 32",
   },
+ 
 };
 
 module.exports = nextConfig;
