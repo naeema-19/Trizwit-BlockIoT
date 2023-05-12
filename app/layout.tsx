2554f1d3def8db0660a0c49c05f5995cb03a4c55
NAEEMA ZIYAD <75922224+naeema-19@users.noreply.github.com>
2023-05-12 17:17:00 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -17,6 +18,17 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-X12NFXBGS2" strategy="afterInteractive"/>
+      <Script id="google-analytics" strategy="afterInteractive">
+      {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-X12NFXBGS2', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <head />
 
       <body className="dark:bg-black">
